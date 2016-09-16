79a1bc1d00ed13bf4cf522ee30d1256b6fd28bab
agus sukoco <agussukoco16@gmail.com>
2016-09-16 13:53:36 +0700

    source code HoG
    Fadil tolong dilengkapi yah

---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.GPU;
+
+namespace Detection
+{
+    public partial class Form1 : Form
+    {
+        private Capture _capture = null;
+        private bool _captureProgress;
+        float X, Y;
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                _capture = new Capture();
+                _capture.ImageGrabbed += ProcessFrame;
+            }
+            catch (NullReferenceException excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+        }
+
+        public void ProcessFrame(object sender, EventArgs arg)
+        {
+            Image<Bgr, Byte> frame = _capture.RetrieveBgrFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_LINEAR).Copy(); ;
+            int detected_human = 0;
+            long processingTime;
+            MCvFont f = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_COMPLEX_SMALL, 0.6, 0.7);
+
+            List<Rectangle> dataBound = new List<Rectangle>();
+            List<string> dataTrain = new List<string>();
+
+            string fileSawi = "D:\\sawi.txt";
+            Size winSizeSawi = new Size(64, 128);
+            Rectangle[] resultCabinet = findObject.findObjects(frame, out processingTime, winSizeSawi, fileSawi);
+            foreach (Rectangle rect in resultCabinet)
+            {
+                ++detected_human;
+                frame.Draw(rect, new Bgr(Color.Red), 2);
+                Point HOGLocation = rect.Location;
+                X = HOGLocation.X;
+                Y = HOGLocation.Y;
+
+                frame.Draw("[" + detected_human + "]" + "botol", ref f, new Point(rect.Left, rect.Top - 5), new Bgr(Color.Yellow));
+            }
+
+            imageBox1.Image = frame;
+            
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            if(_capture != null)
+            {
+                if(_captureProgress)
+                {
+                    btn_start.Text = "start";
+                    _capture.Pause();
+                }
+                else
+                {
+                    btn_start.Text = "stop";
+                    _capture.Start();
+                }
+                _captureProgress = !_captureProgress;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ findObject.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Diagnostics;
+using Emgu.CV.GPU;
+
+namespace Detection
+{
+    
+    public static class findObject
+    {
+       //=============================SVM Cassifier Data Training Tanaman=========================================
+        private static float[] GetDataObjects(string objectData)
+        {
+            List<float> data = new List<float>();
+            using (System.IO.StreamReader reader = new System.IO.StreamReader(objectData))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    var arr = new float[line.Length];
+                    for (var i=0; i < line.Length; i++)
+                    {
+                        arr[i] = (float)Convert.ToDouble(line[i]);
+                        data.Add(arr[i]);
+                    }
+                }
+            }
+            var array = data.ToArray();
+            return array;
+        }
+        static Size blockSize = new Size(16, 16);
+        static Size blockStride = new Size(8, 8);
+        static Size winStride = new Size(8, 8);
+        static Size cellSize = new Size(8, 8);
+        static int nbins = 9;
+
+        //=============================Feature Descriptor (HOG) Data Training Tanaman=============================
+        public static Rectangle[] findObjects(Image<Bgr, Byte> image, out long processingTime, Size winSize, string dataFile)
+        {
+            Stopwatch watch;
+            Rectangle[] regions;
+            if(GpuInvoke.HasCuda)
+            {
+                using (GpuHOGDescriptor des = new GpuHOGDescriptor())
+                {
+                    des.SetSVMDetector(GpuHOGDescriptor.GetDefaultPeopleDetector());
+
+                    watch = Stopwatch.StartNew();
+                    using (GpuImage<Bgr, Byte> gpuImg = new GpuImage<Bgr,byte>(image))
+                    using (GpuImage<Bgra,Byte> gpuBgra = gpuImg.Convert<Bgra,Byte>())
+                    {
+                        regions = des.DetectMultiScale(gpuBgra);
+                    }
+                }
+            }
+            else
+            {
+                using (HOGDescriptor des = new HOGDescriptor(winSize, blockSize, blockStride, cellSize, nbins, 1, -1, 0.2, true)) 
+                {
+                    des.SetSVMDetector(GetDataObjects(dataFile));
+                    watch = Stopwatch.StartNew();
+                    regions = des.DetectMultiScale(image);
+                }
+            }
+            watch.Stop();
+            processingTime = watch.ElapsedMilliseconds;
+            return regions;
+        }
+    }
+}
